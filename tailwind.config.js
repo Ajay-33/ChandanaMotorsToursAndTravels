bc01adea248bddd875544a0898e94908a4429715
Dornala Ajay <ajaydornala1729@gmail.com>
2025-09-15 04:23:20 +0530

    Initialized the repo with a node.js environment for tailwind utility setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./*.html",        // all HTML files in root
+    "./src/**/*.js",   // all JS files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
