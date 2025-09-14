ac9f1327d034077fb7798c730c956d8c130c09e0
Dornala Ajay <ajaydornala1729@gmail.com>
2025-09-15 04:28:09 +0530

    Added EmailJS config for deployment

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+// This file contains the configuration keys for EmailJS.
+// It should NOT be committed to version control.
+
+const emailjsConfig = {
+    publicKey: 'ebKZhWCmpYszhM6QL',
+    serviceID: 'service_j99ja7c',
+    templateID: 'template_sx16hok'
+};
